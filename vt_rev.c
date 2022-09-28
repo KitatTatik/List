11d26eaa5444f6f5ed69b80c6e104cb74b1c6341
KitatTatik <113123468+KitatTatik@users.noreply.github.com>
2022-09-28 21:30:56 +0300

    Add files via upload
    recursion

---
+++ vt_rev.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#undef DEBUG
+
+typedef struct List ListType;
+
+struct List {
+    ListType *next;
+    int i;
+};
+
+void print_list(ListType *head) {
+    if (!head) 
+        return;
+
+    printf("%d\n", head->i);
+    print_list(head->next);
+}
+
+ListType* create_lep(int i) {
+    ListType *lep = (ListType*) malloc(sizeof(ListType));
+
+    lep->i = i;
+    lep->next = (ListType*) 0;
+}
+
+#define LIST_SIZE 10 // must be > 0
+
+ListType* gen_list(void) {
+    ListType *head,
+             *cur_lep,
+             *next_lep;
+    int i;
+
+    head = create_lep(10);
+
+    cur_lep = head;
+    for (i = 1; i < LIST_SIZE ; i++) { 
+        next_lep = create_lep(i + 10);
+
+        cur_lep->next = next_lep;
+        cur_lep = next_lep;
+    }
+
+    return head;
+}
+
+void reverse_list_recursive(ListType **head) {
+// TODO: implement this function
+    ListType * rosh; 
+    ListType * zanav; 
+    ListType *next;  
+		        
+    if (*head == NULL) {
+        return;
+    }
+    rosh = *head;        
+    zanav = rosh->next;  //  tail shifted 
+#ifdef DEBUG
+    printf("DEBUG BEFORE rosh->next = %llu\n", (long long unsigned int) rosh->next);
+#endif
+ 
+    if (zanav == NULL) {
+        return;
+    }
+    reverse_list_recursive(&zanav);  // repeat with the tail, that is now shorter by 1
+    rosh->next->next = rosh;   // set first to the end of list
+    
+#ifdef DEBUG
+    printf("DEBUG AFTER rosh->next = %llu\n", (long long unsigned int) rosh->next);
+#endif
+    rosh->next = NULL;         // zeroing of the first
+    *head = zanav;             // fixing the head
+}
+
+ListType* reverse_list_iterative(ListType *head) {
+// TODO: implement this function
+
+    ListType  *cur,
+              *prev,
+              *next;
+
+    if (!head || !head->next) // lazy evaluation (?)
+//    if (!head->next || !head) // wrong!!!
+        return head;
+
+    prev = head;
+    cur = head->next;
+    head->next = 0;
+
+    for (;;) {
+#ifdef DEBUG
+        printf("DEBUG cur->i = %d\n", cur->i);
+#endif
+        next = cur->next;
+        cur->next = prev;
+
+        if (!next) break;
+
+        prev = cur;
+        cur = next;
+    }
+#ifdef DEBUG
+    printf("DEBUG cur->next = %llu\n", (long long unsigned int) cur->next);
+#endif
+
+    return cur;
+}
+
+void main(void) {
+
+    ListType *head = gen_list(); 
+    
+    printf("INITIAL LIST: \n");
+    print_list(head);
+
+    reverse_list_recursive(&head);
+    printf("RECURSIVELY REVERSED:\n");
+//    printf("-----\n");
+    print_list(head);
+
+    head = reverse_list_iterative(head);
+    printf("ITERATIVELY REVERSED:\n");
+//    printf("-----\n");
+    print_list(head);
+
+    reverse_list_iterative(0);
+}
+
